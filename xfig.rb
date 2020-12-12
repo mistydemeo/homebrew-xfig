ea3dd94f605cb94734d3730c189f7bb878c00e17
Yann-R <yann.ricquebourg@irisa.fr>
2020-12-12 15:35:01 +0100

    Bump to version 3.2.7b

--- xfig.rb
+++ xfig.rb
@@ -1,8 +1,8 @@
 class Xfig < Formula
   desc "Interactive drawing tool for X"
   homepage "https://mcj.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/mcj/xfig-3.2.7a.tar.xz"
-  sha256 "ca89986fc9ddb9f3c5a4f6f70e5423f98e2f33f5528a9d577fb05bbcc07ddf24"
+  url "https://downloads.sourceforge.net/project/mcj/xfig-3.2.7b.tar.xz"
+  sha256 "bbc1c4aa8eb7fc06559a5bf83c4798f022be413519778a5d5cdccd24f39a0bce"
 
   depends_on "fig2dev"
   depends_on "ghostscript"
